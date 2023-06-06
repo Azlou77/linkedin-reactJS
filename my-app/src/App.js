136e717ef1f6e7c23a4af21d2d387bedc8ee5b84
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2023-06-06 14:47:18 +0000

    pass data by props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
+import logoPink from '.svg '
 import './App.css';
 
+// Function to open an link
 const AppLink = () => {
   const variable = "React";
   return(<a
@@ -11,17 +13,27 @@ const AppLink = () => {
   >
     Learn (variable) (9*2)+
 </a>)
-ù
+}
+
+// Function to display logo
+
+const Logo = () => <img src={"svg"} className="App-logo" alt="logo" />
+
   
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      
+        <Logo svg="logo"/>
+        <Logo svg="logo"/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        </div>
         
           Learn React
         </a>
