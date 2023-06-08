d6f973707c2c7621a9c9c37b10696e0ae9583cf2
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2023-06-08 13:18:56 +0000

    update pass data by props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import logoPink from '.svg '
+import logoPink from './logo-pink.svg';
 import './App.css';
 
 // Function to open an link
@@ -11,32 +11,24 @@ const AppLink = () => {
     target="_blank"
     rel="noopener noreferrer"
   >
-    Learn (variable) (9*2)+
+    Learn {variable} {9*2}+
 </a>)
 }
 
 // Function to display logo
+const Logo = ({svg}) => <img src={svg} className="App-logo" alt="logo" />
 
-const Logo = () => <img src={"svg"} className="App-logo" alt="logo" />
-
-  
-
+// Function App to display the page
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
-        <Logo svg="logo"/>
-        <Logo svg="logo"/>
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo svg={logo}/>
+        <Logo svg={logoPink}/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-
-        </div>
-        
-          Learn React
-        </a>
+      <AppLink />
       </header>
     </div>
   );
